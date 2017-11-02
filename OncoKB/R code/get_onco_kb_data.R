a6d94d233ae87a271fb4e8dae3637b0af97c30d9
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-11-02 15:45:42 -0400

    script to get all oncokb data. started automation  parsing pmkb data

---
+++ OncoKB/R code/get_onco_kb_data.R
@@ -0,0 +1,20 @@
+library(jsonlite)
+
+# data download from http://oncokb.org/#/dataAccess
+
+# get data and convert to dataframe
+onco_genes <- jsonlite::fromJSON("http://oncokb.org/api/v1/genes")
+onco_evidences_lookup  <- jsonlite::fromJSON("http://oncokb.org/api/v1/evidences/lookup?source=oncotree")
+onco_variants  <- jsonlite::fromJSON("http://oncokb.org/api/v1/variants")
+onco_levels  <- jsonlite::fromJSON("http://oncokb.org/api/v1/levels")
+onco_all_actionable_variants  <- jsonlite::fromJSON("http://oncokb.org/api/v1/utils/allActionableVariants")
+onco_all_annotated_variants  <- jsonlite::fromJSON("http://oncokb.org/api/v1/utils/allAnnotatedVariants")
+
+#write to csv
+write.csv(onco_genes, file = "oncokb_genes.csv")
+write.csv(onco_evidences_lookup, file = "oncokb_evidences_lookup.csv")
+write.csv(onco_variants, file = "oncokb_variants.csv")
+write.csv(onco_levels, file = "oncokb_levels.csv")
+write.csv(onco_all_actionable_variants, file = "oncokb_all_actionable_variants.csv")
+write.csv(onco_all_annotated_variants, file = "oncokb_all_annotated_variants.csv")
+
---
+++ OncoKB/getOnco.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#script to get data from api and convert to csv for onko kb
+
+
+#get oncotree mainTypes
+curl -X GET --header "Accept: application/json" "http://oncotree.mskcc.org/oncotree/api/mainTypes" >oncotree_maintypes.json
+in2csv oncotree_maintypes.json >  oncotree_maintypes.csv -k 'data'
+
+
+# tumor types .txt files
+wget http://oncotree.mskcc.org/oncotree/api/tumor_types.txt
+
+# get genes
+curl http://oncokb.org/api/v1/genes >onco_genes.json
+in2csv onco_genes.json >  onco_genes.csv
+
+#get oncokb evidences
+curl http://oncokb.org/api/v1/evidences/lookup?source=oncotree >onco_evidences.json
+in2csv onco_evidences.json >  onco_evidences.csv
+
+#get oncokb evidences
+curl http://oncokb.org/api/v1/variants >onco_variants.json
+in2csv onco_variants.json >  onco_variants.csv
+
+#get oncokb levels
+curl http://oncokb.org/api/v1/levels >onco_levels.json
+in2csv onco_levels.json >  onco_levels.csv
+
+#get oncokb all actionable variants
+curl http://oncokb.org/api/v1/utils/allActionableVariants >onco_actionableVariants.json
+in2csv onco_actionableVariants.json >  onco_actionableVariants.csv
+
+#get oncokb all annotated variants
+curl http://oncokb.org/api/v1/utils/allActionableVariants >onco_all_annotated_variants.json
+in2csv onco_all_annotated_variants.json >  onco_all_annotated_variants.csv
+
+
+
+# delete un-needed json files
+rm *.json
